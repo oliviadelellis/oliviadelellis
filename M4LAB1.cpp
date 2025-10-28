c88ddf008768c9dbad8c561ceeaa7acdc6ecdef2
Olivia DeLellis <delellio6187@student.faytechcc.edu>
2025-10-28 14:40:00 +0000

    started M4LAB1

---
+++ M4LAB1.cpp
@@ -0,0 +1,21 @@
+/*
+CSC 134
+M4LAB1
+Olivia DeLellis
+10/28
+*/
+
+#include <iostream>
+using namespace std; 
+
+int main (){
+
+    // declare variable
+    int firstNum, secondNum, answer; 
+    firstNum = 2;
+    secondNum= 1; 
+    answer = firstNum * secondNum; 
+
+    // A sample message, without variables 
+    cout << "2 time 1 is 2\n";
+}
