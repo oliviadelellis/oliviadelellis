33ba702e5e8e2e27016f6f9f48847c8f68c906d6
Olivia DeLellis <delellio6187@student.faytechcc.edu>
2025-11-02 15:19:07 -0500

    Implement multiplication table in main function
    Refactor main function to include multiplication table logic

--- M4HW1.cpp
+++ M4HW1.cpp
@@ -5,11 +5,19 @@ Olivia DeLellis
 11/02/2025
 */
 
+
 #include <iostream>
 using namespace std; 
 
-int main () {
+int main() {
 
-    // A sample message, without variables 
-    cout << "2 times 1 is 2\n"; 
-}
+   // declare variables 
+   int firstNum, secondNum, answer; 
+   firstNum = 1; 
+   // secondNum = 1; 
+   // answer = firstNum * secondNum; 
+   
+    for (int i=1; i<= 12; i++) {
+    answer = firstNum * i; 
+    cout << firstNum << " times " << i << " is " << answer << endl; 
+    }
