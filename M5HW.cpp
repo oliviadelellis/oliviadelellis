a2460d937ed93abc8253c3e230d295d23833899a
Olivia DeLellis <delellio6187@student.faytechcc.edu>
2025-11-13 20:16:16 +0000

    deleted M5HW

--- M5HW.cpp
+++ M5HW.cpp
@@ -4,6 +4,35 @@ M5HW
 Olivia DeLellis
 Nov 13, 2025
 */
+int main() {
+    string month1, month2, month3;
+    double rain1, rain2, rain3;
+
+    cout << "Enter month: ";
+    cin >> month1;
+    cout << "Enter rainfall for " << month1 << ": ";
+    cin >> rain1;
+
+    cout << "Enter month: ";
+    cin >> month2;
+    cout << "Enter rainfall for " << month2 << ": ";
+    cin >> rain2;
+
+    cout << "Enter month: ";
+    cin >> month3;
+    cout << "Enter rainfall for " << month3 << ": ";
+    cin >> rain3;
+
+    double avg = (rain1 + rain2 + rain3) / 3.0;
+
+    cout << fixed << setprecision(2);
+    cout << "The average rainfall for " 
+         << month1 << ", " << month2 << ", and " << month3
+         << " is " << avg << " inches.\n";
+
+    return 0;
+}
+
 
 
 
